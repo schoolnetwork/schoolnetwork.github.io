5511d8ebeb54b2d66c604412b2c4623de9a28cc1
schoolnetwork <144613531+schoolnetwork@users.noreply.github.com>
2024-11-18 20:01:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,63 +2,58 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
-// Set up the app and server
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files (your chat page)
-app.use(express.static('public'));
+let messages = [];  // Store messages here
 
-// Keep track of messages and users
-let messages = [];
-let users = {};
+app.use(express.static('public'));  // Serve static files
 
-// When a client connects to the WebSocket server
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Send all messages to the new user
-  socket.emit('load messages', messages);
-
-  // Event to change username
-  socket.on('change username', (username) => {
-    users[socket.id] = username;
-    console.log(`${socket.id} changed username to ${username}`);
-  });
-
-  // Event to receive chat message and broadcast to all clients
-  socket.on('chat message', (msg) => {
-    const username = users[socket.id] || 'Anonymous';
-    const messageData = {
-      user: username,
-      msg: msg,
-      id: socket.id + Date.now() // Unique ID based on socket ID and timestamp
-    };
-    messages.push(messageData);
-    io.emit('chat message', messageData); // Broadcast message to everyone
-  });
-
-  // Event to delete a message (for the owner only)
-  socket.on('delete message', (msgId, password) => {
-    if (password === 'owneriscool') {
-      // Only allow deletion for the owner
-      messages = messages.filter((msg) => msg.id !== msgId);
-      io.emit('load messages', messages); // Reload messages for all clients
-      console.log(`Message with ID ${msgId} deleted by the owner`);
-    } else {
-      console.log('Incorrect password for deletion');
-    }
-  });
-
-  // Disconnect event
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-    delete users[socket.id];
-  });
+// Serve the chat page
+app.get('/chat.html', (req, res) => {
+    res.sendFile(__dirname + '/public/chat.html');
 });
 
-// Start the server
+io.on('connection', (socket) => {
+    console.log('A user connected: ' + socket.id);
+
+    // Send all previous messages to the newly connected client
+    socket.emit('load messages', messages);
+
+    // Listen for incoming messages
+    socket.on('chat message', (msgData) => {
+        // Generate a unique ID for each message
+        const messageId = socket.id + '-' + Date.now();
+
+        // Store the message with the ID
+        const message = {
+            id: messageId,
+            user: msgData.user,
+            msg: msgData.msg,
+        };
+        messages.push(message);
+
+        // Broadcast the message to all clients
+        io.emit('chat message', message);
+    });
+
+    // Listen for owner requesting message deletion
+    socket.on('delete message', (messageId, password) => {
+        if (password === 'owneriscool') {
+            // Delete the message from the array
+            messages = messages.filter(msg => msg.id !== messageId);
+
+            // Broadcast the delete event to all clients
+            io.emit('delete message', messageId);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected: ' + socket.id);
+    });
+});
+
 server.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });
