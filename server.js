906e29737ec1c6a30a1dc8f41948fcd4d8c0fb8a
schoolnetwork <144613531+schoolnetwork@users.noreply.github.com>
2024-11-18 19:46:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,18 +10,47 @@ const io = socketIo(server);
 // Serve static files (your chat page)
 app.use(express.static('public'));
 
+// Keep track of messages and users
+let messages = [];
+let users = {};
+
 // When a client connects to the WebSocket server
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Broadcast incoming messages to all clients
+  // Send all messages to the new user
+  socket.emit('load messages', messages);
+
+  // Event to change username
+  socket.on('change username', (username) => {
+    users[socket.id] = username;
+    console.log(`${socket.id} changed username to ${username}`);
+  });
+
+  // Event to receive chat message and broadcast to all clients
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);  // send to all clients
+    const username = users[socket.id] || 'Anonymous';
+    const messageData = { user: username, msg: msg, id: socket.id };
+    messages.push(messageData);
+    io.emit('chat message', messageData);
+  });
+
+  // Event to delete a message (for the owner only)
+  socket.on('delete message', (msgId, password) => {
+    if (password === 'owneriscool') {
+      // Only allow deletion for the owner
+      messages = messages.filter((msg) => msg.id !== msgId);
+      io.emit('load messages', messages); // Reload messages for all clients
+      console.log(`Message with ID ${msgId} deleted by the owner`);
+    } else {
+      console.log('Incorrect password for deletion');
+    }
   });
 
   // Disconnect event
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    delete users[socket.id];
   });
 });
 
